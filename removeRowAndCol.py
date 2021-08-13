2ffde632dd61023d0bd5c97f700baae87de6a141
hemalathanaredla <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-08-13 12:00:31 +0530

    removeRowAndCol

--- removeRowAndCol.py
+++ removeRowAndCol.py
@@ -23,6 +23,21 @@
 
 def removeRowAndCol(L, row, col):
     # Your code goes here...
-    pass
+    if(row<0 or col<0 or len(L)==0 or len(L)<row or len(L)<col):
+        return None
+    else:
+        del L[row]
+        for i in range(0,len(L)):
+            del L[i][col]
+        return L
+
+   
 
 # Write your own test cases.
+
+assert(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],1,0)==[[3,4,5],[1,2,3]])
+assert(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],1,2)==[[2,3,5],[0,1,3]])
+assert(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],-1,-2)==None)
+assert(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],6,7)==None)
+assert(removeRowAndCol([],1,2)==None)
+print("All test cases passed ..........")
