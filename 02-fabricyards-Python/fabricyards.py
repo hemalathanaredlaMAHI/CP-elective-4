b7fa99f828ea788a62c8792e4067fda76859c6dd
hemalathanaredla <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-08-16 09:01:45 +0530

    fabricyards

--- 02-fabricyards-Python/fabricyards.py
+++ 02-fabricyards-Python/fabricyards.py
@@ -14,8 +14,20 @@
 
 def fabricyards(inches):
 	# Your code goes here...
-	return 1
+	if(inches==0):
+		return 0
+	elif(inches%36==0):
+		return inches//36
+	else:
+		return inches//36+1
+
 
 def fabricexcess(inches):
 	# Your code goes here...
-	return 1
+	if(inches==0):
+		return 0
+	elif(inches%36==0):
+		return 0
+	else:
+		return 36-(inches%36)
+	
