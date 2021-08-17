e4f522af6e371fc687efbde48f59c9fb44add34e
hemalathanaredla <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-08-17 11:34:16 +0530

    linkedlist

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -8,6 +8,9 @@ particular value.
 Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
+from typing import Counter
+
+
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -18,26 +21,61 @@ class LinkedList(object):
         self.head = head
         
     def append(self, new_element):
-        # Your code goes here
-        pass
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
-        # Your code goes here
-        pass
+        p=1
+        c=self.head
+        if(position<1):
+            return None
+        while(c and p<=position):
+            if(p==position):
+                return c
+            c=c.next
+            p=p+1
+        return c
+
+
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        # Your code goes here
-        pass
-    
-    
+        p=1
+        c=self.head
+        if(position==1):
+            new_element.next=self.head
+            self.head=new_element
+        else:
+            while(c and p<position):
+                if(p==position-1):
+                    new_element.next=c.next
+                    c.next=new_element
+                c=c.next
+                p=p+1
+
+
+
     def delete(self, value):
         """Delete the first node with a given value."""
-        # Your code goes here
-        pass
+        c=self.head
+        b=None
+        while(c.next):
+            if(c.value==value):
+                if(b):
+                    b.next=c.next
+                else:
+                    self.head=c.next
+            b=c
+            c=c.next
+
