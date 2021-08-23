59db93e17b7d12960613eeca87b245a35e3481ff
hemalathanaredlaMAHI <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-08-23 10:52:36 +0530

    canqueenattack

--- canqueenattack.py
+++ canqueenattack.py
@@ -1,1 +1,13 @@
+  
+# canQueenAttack(qX, qY, oX, oY) [15 pts]
+# Given the position of the queen (qX, qY) and the opponent (oX, oY) on a chessboard. The task is to determine 
+# whether the queen can attack the opponent or not. Note that the queen can attack in the same row, same column and 
+# diagonally.
 
+def canqueenattack(qR, qC, oR, oC):
+  if(qR==oR or qC==oC):
+    return True
+  elif((qR-qC)==(oR-oC)):
+    return True
+  else:
+    return False
