6845d43aec703c2d24d73a09b1ac25d65b39ea4c
hemalathanaredlaMAHI <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-08-25 08:52:41 +0530

    LatinSquare

--- LatinSquare.py
+++ LatinSquare.py
@@ -8,5 +8,27 @@
 # Write your own test cases...
 
 def isLatinSquare(lst):
-    # Your code goes here...
-    pass
+    if(len(lst)==0):
+        return False
+    d=[i for i in range(1,len(lst)+1)]
+    for i in lst:
+        for j in d:
+            if(j not in i):
+                return False
+    for i in range(0,len(lst)):
+        t=[]
+        for j in range(0,len(lst)):
+            t.append(lst[j][i])
+        for k in d:
+            if k not in t:
+                return False
+    return True
+
+
+assert(isLatinSquare([[1,2,3],[2,3,1],[3,1,2]])==True)
+assert(isLatinSquare([[1,1,1],[1,1,1],[0,0,0]])==False)
+assert(isLatinSquare([[1,2,3,4],[2,3,4,1],[3,4,1,2],[4,1,2,3]])==True)
+assert(isLatinSquare([])==False)
+assert(isLatinSquare([[1,2,3],[4,5,6],[7,8,9]])==False)
+assert(isLatinSquare([[1,2,3],[1,2,3],[1,2,3]])==False)
+print("All Test cases passed..................")
