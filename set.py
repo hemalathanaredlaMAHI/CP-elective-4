3b2a9fdd211b6e4658b71255cab19e392561e9ef
hemalathanaredlaMAHI <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-08-25 09:05:30 +0530

    limitedPowerSet

--- set.py
+++ set.py
@@ -8,5 +8,30 @@
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
 
 def limitedPowerSet(n, k):
-    # Your code goes here...
-    pass
+    ls=[]
+    ls.append({})
+    for i in range(1,n+1):
+        for j in range(1,(n+1)-(i-1)):
+            temp=[]
+            for x in range(j,j+i):
+                temp.append(x)
+                
+            if(len(ls)<k):
+                ls.append(set(temp))
+            else:
+                return ls
+    if(len(ls)==k):
+        return ls
+    else:
+        return None
+    
+
+
+
+assert(limitedPowerSet(5,16)==[{}, {1}, {2}, {3}, {4}, {5}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {1, 2, 3, 4}, {2, 3, 4, 5}, {1, 2, 3, 4, 5}])
+assert(limitedPowerSet(0,3)==None)
+assert(limitedPowerSet(0,1)==[{}])
+assert(limitedPowerSet(5,3)==[{}, {1}, {2}])
+assert(limitedPowerSet(3,3)==[{}, {1}, {2}])
+assert(limitedPowerSet(7,17)==[{}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}])
+print("All Test cases passed................")
